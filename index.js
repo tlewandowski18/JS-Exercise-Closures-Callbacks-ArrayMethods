49d976c98025f77551846cdb0f75ca7399863538
tlewandowski18 <tlewandowski18@gmail.com>
2019-11-02 12:41:04 -0400

    complete first two challenges

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function processFirstItem(stringList, callback) {
  * [2] Invoking `processLength` passing `[]` and `(num) => "There are " + num`,
  * should return "There are 0".
 */
-function processLength(/* CODE HERE */) {
+function processLength(arr, cb) {
   /* CODE HERE */
+  return cb(arr.length);
 }
 
 /**
@@ -66,8 +67,9 @@ function processLength(/* CODE HERE */) {
  * Invoking `processLastItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'barbar'.
 */
-function processLastItem(/* CODE HERE */) {
+function processLastItem(stringList, cb) {
   /* CODE HERE */
+  return cb(stringList[stringList.length - 1]);
 }
 
 /**
